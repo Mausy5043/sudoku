11f20c322a8a70a0cae4a388d1b088af64f1a1a4
Mausy5043 <Mausy5043@users.noreply.github.com>
2018-04-01 13:42:51 +0200

    fix E701: multiple statements on one line (colon)

--- sudoku.py
+++ sudoku.py
@@ -15,7 +15,8 @@ def sd_genmat():
 
 def sd_update(R, C, sr, sc, r, v):
   m, m_c = 10, 0
-  for c2 in range(4): sc[C[r][c2]] += v<<7
+  for c2 in range(4):
+    sc[C[r][c2]] += v<<7
   for c2 in range(4):
     c = C[r][c2]
     if v > 0:
@@ -26,7 +27,8 @@ def sd_update(R, C, sr, sc, r, v):
           for cc2 in range(4):
             cc = C[rr][cc2]
             sc[cc] -= 1
-            if sc[cc] < m: m, m_c = sc[cc], cc
+            if sc[cc] < m:
+              m, m_c = sc[cc], cc
     else:
       for r2 in range(9):
         rr = R[c][r2]
@@ -43,8 +45,10 @@ def sd_solve(R, C, s):
   cr = [-1 for i in range(81)]
   cc = [-1 for i in range(81)]
   for i in range(81):
-    if ord(s[i]) >= 49 and ord(s[i]) <= 57: a = ord(s[i]) - 49
-    else: a = -1
+    if ord(s[i]) >= 49 and ord(s[i]) <= 57:
+      a = ord(s[i]) - 49
+    else:
+      a = -1
     if a >= 0:
       sd_update(R, C, sr, sc, i * 9 + a, 1)
       hints += 1
@@ -57,21 +61,27 @@ def sd_solve(R, C, s):
           for c in range(324):
             if sc[c] < m:
               m, cc[i] = sc[c], c
-              if m < 2: break
+              if m < 2:
+                break
         if m == 0 or m == 10:
           cr[i], d, i = -1, -1, i - 1
       c = cc[i]
-      if d == -1 and cr[i] >= 0: sd_update(R, C, sr, sc, R[c][cr[i]], -1)
+      if d == -1 and cr[i] >= 0:
+        sd_update(R, C, sr, sc, R[c][cr[i]], -1)
       r2_ = 9
       for r2 in range(cr[i] + 1, 9):
-        if sr[R[c][r2]] == 0: r2_ = r2; break
+        if sr[R[c][r2]] == 0:
+          r2_ = r2; break
       if r2_ < 9:
         m, cc[i+1] = sd_update(R, C, sr, sc, R[c][r2], 1)
         cr[i], d, i = r2, 1, i + 1
-      else: cr[i], d, i = -1, -1, i - 1
-    if i < 0: break
+      else:
+        cr[i], d, i = -1, -1, i - 1
+    if i < 0:
+      break
     y = []
-    for j in range(81): y.append(out[j])
+    for j in range(81):
+      y.append(out[j])
     for j in range(i):
       r = R[cc[j]][cr[j]]
       y[r//9] = r%9 + 1
